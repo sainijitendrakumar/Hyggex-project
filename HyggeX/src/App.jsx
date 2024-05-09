13d17822760ba3e2c3ecc894df2bef311933bc35
Jitendra Kumar Saini <65939231+sainijitendrakumar@users.noreply.github.com>
2024-05-09 16:39:00 +0530

    Add files via upload

---
+++ HyggeX/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import HyggeX from './pages/HyggeX';
+
+export default function App() {
+  return (
+    <HyggeX />
+  );
+}
---
+++ HyggeX/src/pages/HyggeX.jsx
@@ -0,0 +1,207 @@
+import "./yes.jpg"
+
+
+export default function HyggeX() {
+  return (
+    <div className="bg-[#F9F9F9] flex flex-col p-[1.7rem_6.4rem_19.2rem_6.5rem] w-[90rem] box-sizing-border font-sans">
+      <div className="m-[0_0rem_4.2rem_0] flex flex-row justify-between w-[77.1rem] box-sizing-border font-sans">
+        <div className="flex flex-row items-center">
+        <img className="w-13 h-10 bg-[#F9F9F9]" src="/yes1.png" alt="logo"/>
+        <p className="text-2xl text-blue-950">Hygge<span className="font-bold">X</span></p>
+        </div>
+        <div className="flex flex-row box-sizing-border">
+          <div className="m-[0.8rem_2.5rem_0.8rem_0] flex flex-row justify-between w-[17.6rem] h-[fit-content] box-sizing-border">
+            <span className=" break-words font-sans font-normal text-[1.1rem] text-[#3A3740]">
+            Home
+            </span>
+            <span className="break-words font-sans font-normal text-[1.1rem] text-[#3A3740]">
+            Flashcard
+            </span>
+            <span className="break-words font-sans font-normal text-[1.1rem] text-[#3A3740]">
+            Contact
+            </span>
+          </div>
+          <div className="m-[0.8rem_2.6rem_0.8rem_0] inline-block break-words font-sans font-normal text-[1.1rem] text-[#3A3740]">
+          FAQ
+          </div>
+          <div className="rounded-[2rem] bg-[linear-gradient(180deg,#06286E,#164EC0)] flex items-center justify-center p-[0.8rem_0rem_0.8rem_0] w-[8rem] box-sizing-border">
+            <span className="break-words font-sans font-medium text-[1.1rem] text-[#FFFFFF]">
+            Login
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="m-[0_0.2rem_4.3rem_0.2rem] flex flex-row self-start w-[fit-content] box-sizing-border">
+        <div className="m-[0_0.6rem_0_0] flex flex-row items-center justify-center box-sizing-border">
+          <div className="flex flex-col items-center justify-center w-[1.3rem] h-[1.3rem] box-sizing-border">
+          <i className="fa-solid fa-house"></i>
+          </div>
+          <div className=" flex flex-col items-center justify-center mx-2 ">
+          &gt;
+          </div>
+          <div className="  break-words font-sans font-medium text-[1.1rem] text-[#696671]">
+          Flashcard
+          </div>
+        </div>
+        <div className=" flex flex-row ">
+          <div className=" flex flex-col items-center justify-center mx-2">
+          &gt;
+          </div>
+          <span className="break-words font-sans font-medium text-[1.1rem] text-[#696671]">
+          Mathematics
+          </span>
+          <div className="flex flex-row ">
+          <div className=" flex flex-col items-center justify-center mx-2">
+          &gt;
+          </div>
+            <span className="break-words font-sans font-semibold text-[1.1rem] text-[#06286E]">
+            Relation and Function
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="m-[0_0.1rem_3.5rem_0.1rem] inline-block self-start break-words font-sans font-bold text-[2rem] bg-[linear-gradient(180deg,#06286E,#164EC0)] text-[transparent] bg-clip-text">
+      Relations and Functions ( Mathematics )
+      </div>
+      <div className="m-[0_0.6rem_1.9rem_0] flex flex-row justify-between self-center w-[34.1rem] box-sizing-border">
+        <div className="flex flex-col items-center box-sizing-border">
+          <div className="m-[0_0.8rem_0.3rem_0.8rem] flex box-sizing-border">
+            <span className="break-words font-sans font-bold text-[1.3rem] text-[#06286E]">
+            Study
+            </span>
+          </div>
+          <div className="bg-[#06286E] w-[5.1rem] h-[0.1rem]">
+          </div>
+        </div>
+        <div className="flex flex-col items-center box-sizing-border">
+          <div className="m-[0_0.8rem_0.3rem_0.8rem] flex box-sizing-border">
+            <span className="break-words font-sans font-[var(--med,500)] text-[1.3rem] text-[#696671]">
+            Quiz
+            </span>
+          </div>
+          <div className="bg-[#F9F9F9] w-[4.3rem] h-[0.1rem]">
+          </div>
+        </div>
+        <div className="flex flex-col items-center box-sizing-border">
+          <div className="m-[0_0.8rem_0.3rem_0.8rem] flex box-sizing-border">
+            <span className="break-words font-sans font-[var(--med,500)] text-[1.3rem] text-[#696671]">
+            Test
+            </span>
+          </div>
+          <div className="bg-[#F9F9F9] w-[4.1rem] h-[0.1rem]">
+          </div>
+        </div>
+        <div className="flex flex-col items-center box-sizing-border">
+          <div className="m-[0_0.8rem_0.3rem_0.8rem] flex box-sizing-border">
+            <span className="break-words font-sans font-[var(--med,500)] text-[1.3rem] text-[#696671]">
+            Game
+            </span>
+          </div>
+          <div className="bg-[#F9F9F9] w-[5rem] h-[0.1rem]">
+          </div>
+        </div>
+        <div className="flex flex-col items-center box-sizing-border">
+          <div className="m-[0_0.8rem_0.3rem_0.8rem] flex box-sizing-border">
+            <span className="break-words font-sans font-[var(--med,500)] text-[1.3rem] text-[#696671]">
+            Others
+            </span>
+          </div>
+          <div className="bg-[#F9F9F9] w-[5.6rem] h-[0.1rem]">
+          </div>
+        </div>
+      </div>
+      <div className="m-[0_0.1rem_5.1rem_0] flex flex-col items-center self-center w-[fit-content] box-sizing-border">
+        <div className="m-[0_0_2rem_0] flex h-[24.6rem] box-sizing-border">
+          <div className="rounded-[2.7rem] bg-[#C4C4C4] relative flex h-[24.6rem] box-sizing-border">
+            <div className="rounded-[2.7rem] bg-[linear-gradient(42.94deg,#051A91_104.3%,#061C93_121.52%,#2284F1_183.61%,#1F80EB_203.8%)] flex flex-row p-[2.3rem_2.4rem_0_2.6rem] h-[24.6rem] box-sizing-border">
+              <div className="m-[0_8.6rem_9.7rem_0] flex w-[1.2rem] h-[1.8rem] box-sizing-border">
+              <i className="fa-regular fa-lightbulb text-white"></i>
+               
+              </div>
+              <div className="m-[8.6rem_8.4rem_0_0] inline-block break-words font-['Lato'] font-bold text-[2.4rem] tracking-[0rem] text-[#FFFFFF]">
+              9 + 6 + 7x - 2x - 3
+              </div>
+              <div className="m-[0.1rem_0_9.8rem_0] flex w-[1.6rem] h-[1.6rem] box-sizing-border">
+              <i className="fa-solid fa-volume-high text-white"></i>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="m-[0_3.1rem_0_3.1rem] flex flex-row justify-between w-[38.3rem] box-sizing-border">
+        <div className=" flex flex-col items-center justify-center rounded-3xl w-[3.1rem] h-[3.1rem] box-sizing-border ">
+          <i className="fa-solid fa-arrow-rotate-right fa-2xl text-center text-blue-700" ></i>
+          </div>
+        
+          <div className="flex flex-row items-center justify-between ">
+          <div className=" flex flex-col items-center justify-center rounded-3xl w-[3.1rem] h-[3.1rem] box-sizing-border bg-[linear-gradient(180deg,#06286E,#164EC0)]">
+          <i className="fa-solid fa-angle-left fa-xl text-white text-center" ></i>
+          </div>
+            <div className="mx-8 inline-block break-words font-sans font-bold text-[1.5rem] text-[#202B37]">
+            01/10
+            </div>
+            <div className=" flex flex-col items-center justify-center rounded-3xl w-[3.1rem] h-[3.1rem] box-sizing-border bg-[linear-gradient(180deg,#06286E,#164EC0)]">
+          <i className="fa-solid fa-angle-right fa-xl text-white text-center" ></i>
+          </div>
+          </div>
+          <div className=" flex flex-col items-center justify-center rounded-3xl w-[3.1rem] h-[3.1rem] box-sizing-border ">
+          <i className="fa-solid fa-expand fa-2xl text-center text-blue-700" ></i>
+          </div>
+         
+        </div>
+      </div>
+      <div className="m-[0_0_9.7rem_0.1rem] flex flex-row justify-between w-[77rem] box-sizing-border">
+        <div className="flex flex-row box-sizing-border">
+          <div className="rounded-full shadow-[0rem_0rem_1.9rem_0rem_rgba(8,56,153,0.4)] m-[0_1.3rem_0_0] flex w-[5rem] h-[5rem] ">
+            <div className="rounded-full bg-[#FFFFFF] w-[5rem] h-[5rem] flex flex-row items-center justify-center">
+            <img className="w-13 h-10 bg-[#F9F9F9]" src="/yes1.png" alt="logo"/>
+            </div>
+          </div>
+          <div className="m-[0_0_1.3rem_0] flex flex-col box-sizing-border">
+            <div className="m-[0_0_0.6rem_0] inline-block self-start break-words font-sans font-bold text-[0.8rem] text-[#696671]">
+            Published by
+            </div>
+            <p className="text-3xl text-blue-950">Hygge<span className="font-bold">X</span></p>
+          </div>
+        </div>
+        <div className="m-[0.9rem_0_1rem_0] flex flex-row box-sizing-border">
+          <div className="m-[0_0.8rem_0_0] flex flex-col items-center justify-center rounded-3xl w-[3.1rem] h-[3.1rem] box-sizing-border bg-[linear-gradient(180deg,#06286E,#164EC0)]">
+          <i className="fa-solid fa-plus fa-xl text-white text-center" ></i>
+          </div>
+          <div className="m-[0.5rem_0_0.5rem_0] inline-block break-words font-sans font-semibold text-[1.8rem] bg-[linear-gradient(180deg,#06286E,#164EC0)] text-[transparent] bg-clip-text">
+          Create Flashcard
+          </div>
+        </div>
+      </div>
+      <div className="m-[0_0_1.6rem_0] inline-block self-start break-words font-sans font-bold text-[3rem] tracking-[0rem] bg-[linear-gradient(180deg,#06286E,#164EC0)] text-[transparent] bg-clip-text">
+      FAQ
+      </div>
+      <div className="flex flex-col items-center self-start w-[53rem] box-sizing-border">
+        <div className="rounded-[0.8rem] border-[0.1rem_solid_#217EEC] m-[0_0_2rem_0] flex flex-row justify-between p-[1rem_1.7rem_0.5rem_1.5rem] w-[53rem] box-sizing-border border-2 border-blue-500">
+          <span className="m-[0_0.8rem_0_0] w-[48rem] break-words font-sans font-semibold text-[1rem] leading-[1.625] text-[#28262C]">
+          Can education flashcards be used for all age groups?
+          </span>
+          <div className="m-[0.1rem_0_1rem_0] flex w-[1rem] h-[0.5rem] box-sizing-border">
+          <i className="fa-solid fa-angle-down"></i>
+          </div>
+        </div>
+        <div className="rounded-[0.8rem] border-[0.1rem_solid_#217EEC] m-[0_0_2rem_0] flex flex-row justify-between p-[1rem_1.7rem_0.5rem_1.5rem] w-[53rem] box-sizing-border border-2 border-blue-500">
+          <span className=" w-[48rem] break-words font-sans font-semibold text-[1rem] leading-[1.625] text-[#28262C]">
+          How do education flashcards work?
+          </span>
+          <div className=" flex w-[1rem] h-[0.5rem] box-sizing-border">
+          <i className="fa-solid fa-angle-down"></i>
+          </div>
+        </div>
+        <div className="rounded-[0.8rem] border-[0.1rem_solid_#217EEC] flex flex-row items-center justify-between p-[1rem_1.7rem_0.5rem_1.5rem] w-[53rem] box-sizing-border border-2 border-blue-500">
+          <span className="m-[0_0.8rem_0_0] w-[48rem] break-words font-sans font-semibold text-[1rem] leading-[1.625] text-[#28262C]">
+          Can education flashcards be used for test preparation?
+          </span>
+          <div className=" flex w-[1rem] h-[0.5rem] box-sizing-border">
+          <i className="fa-solid fa-angle-down"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
